ef67f271e2bf3293063cff65574ddcbc5156bacd
Thibaud Briard <thibaud.brrd@eduge.ch>
2021-09-29 16:01:42 +0200

    adding timer

---
+++ src/components/Timer.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="timer">
+    <h1>timer Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'timer',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .timer {
+
+  }
+</style>
