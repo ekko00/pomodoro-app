2447da95ff4de76447f13f04769b540a6a2c8333
Thibaud Briard <thibaud.brrd@eduge.ch>
2021-10-06 13:22:39 +0200

    Creating Timer properties and methods

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -7,19 +7,77 @@
 </template>
 
 <script lang="js">
+  const WORK_TIME_DEFAULT = 1200;
+  const REST_TIME_DEFAULT = 300;
+  const STATE_DEFAULT = "work";
 
   export default  {
     name: 'timer',
     props: [],
     mounted () {
-
+      state = STATE_DEFAULT;
+      if (state = "work") {
+        time = WORK_TIME_DEFAULT;
+      }
+      else {
+        time = REST_TIME_DEFAULT;
+      }
     },
     data () {
       return {
-
+        clock: Number;
+        time: Number;
+        state: String;
       }
     },
     methods: {
+      clockProcess() {
+        // If the count down is over, write some text 
+        if (time <= 0) {
+          swapState();
+        }
+        else {
+          decrementTime();
+        }
+      
+        // Show time
+        showTime();
+      }
+
+      playTimer() {
+        clock = setInterval(clockProcess, 1000);
+      }
+
+      pauseTimer() {
+        clearInterval(clock);
+      }
+
+      stopTimer() {
+        clearInterval(clock);
+        time = workTime;
+        state = "work";
+      }
+      
+      showTime() {
+        // Time calculations for minutes and seconds
+        var minutes = Math.floor((time % (60 * 60)) / 60);
+        var seconds = Math.floor((time % (60)));
+      }
+
+      decrementTime() {
+        time--;
+      }
+
+      swapState() {
+        if (state == "work") {
+          time = restTime;
+          state == "rest";
+        }
+        else {
+          time = workTime;
+          state = "work";
+        }
+      }
 
     },
     computed: {
